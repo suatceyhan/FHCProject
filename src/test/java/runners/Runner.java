cd5fc2688ceec2198c023d0b53fb205af20836b8
suatceyhan <suatceyhan@gmail.com>
2020-08-31 23:02:26 -0400

    us04 halledildi

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@userstory04",  //"@fhcproject",
+        tags = "@fhcproject",
         dryRun = false
 )
 
